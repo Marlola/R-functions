b5c10a0ecb8928f0490433061bef8a276437dd5d
fg-code <florian.gollnow@geo.hu-berlin.de>
2015-02-03 10:49:07 +0100

    stratified sampling bug fix

--- StratifiedRandomSamplingDistance.R
+++ StratifiedRandomSamplingDistance.R
@@ -29,22 +29,22 @@ StratifiedRandomDistance <- function(x, min.dist,  sample.n, progress=TRUE){
   pb <- txtProgressBar(min = 0, max = 100, style = 3)
   classes <- sort(unique(x))  # which unique classes               
   sample.size<- rep(sample.n, length(classes)) # vector of sample sizes to be compared to
+  current.sample <- rep(0, length(classes)) # initial
   sample.init <- sampleRandom(x, 1, sp=FALSE, xy=TRUE) #initial sample
-  current.sample <- rep(0, length(classes)) # initial 
   repeat{
     sample.tmp <- sampleRandom(x, 1, sp=FALSE, xy=TRUE) # temporary sample.init
     dist<- pointDistance(sample.init[,c(1,2)], sample.tmp[,c(1,2)], lonlat=FALSE) # distant calculation
     #print(current.sample)
     if (all(dist >= min.dist) & sample.size[which(classes==sample.tmp[,3])] > current.sample[which(classes==sample.tmp[,3])]){ # add if min dist and sample.init size allows
       sample.init <- rbind(sample.init, sample.tmp)
-      current.sample <- tabulate(sample.init[,3], nbins=length(sample.size)) # how many samples are there 
-      #print(current.sample)
+      current.sample <- tabulate(sample.init[,3], nbins=max(classes))[classes] # how many samples are there 
+  #    print(current.sample)
       if (progress==TRUE){
         setTxtProgressBar(pb, (sum(current.sample)/sum(sample.size))*100)
-        }
       }
+    }
     if (sample.size[which(classes==sample.tmp[,3])] == current.sample[which(classes==sample.tmp[,3])]){
-      #print(paste ("set",sample.tmp[,3], "to NA")
+      print(paste ("set",sample.tmp[,3], "to NA"))
       x[Which(x==sample.tmp[,3])] <- NA
     }
     if (all(current.sample == sample.size)){ # stop when all sample sizes are fullfilled
